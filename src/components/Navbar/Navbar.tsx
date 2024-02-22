b8052ff6ee661c29df70b892463b84049707496d
Neider Lopez <lpzneider@gmail.com>
2024-02-21 21:04:24 -0500

    first commit

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+export type NavbarProps = {
+  // types...
+};
+
+const Navbar: React.FC<NavbarProps> = ({}) => {
+  return <div> Navbar works! </div>;
+};
+
+export default Navbar;

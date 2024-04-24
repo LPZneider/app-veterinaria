283eb0c73d120bef1d3ee32789df7d5c643ce635
Neider Lopez <lpzneider@gmail.com>
2024-04-23 23:33:04 -0500

    listo editar mascota

--- src/services/mascotaEdit.service.ts
+++ src/services/mascotaEdit.service.ts
@@ -1,6 +1,7 @@
 import { EditMascota } from "@/models/editMascota";
 import { loadAbort } from "@/utilities";
 import axios from "axios";
+import dayjs from "dayjs";
 
 const editMascota = ({
   id,
@@ -10,21 +11,15 @@ const editMascota = ({
   fechaNacimiento,
 }: EditMascota) => {
   const controller = loadAbort();
+  console.log(fechaNacimiento);
+  const fecha = dayjs(fechaNacimiento);
+  const formattedDate = fecha.format("YYYY-MM-DD");
   return {
     call: axios.put(
-      "http://localhost:8080/veterinaria/mascotas",
-      {
-        id,
-        nombre,
-        idRaza,
-        idPropietario,
-        fechaNacimiento,
-      },
+      `http://localhost:8080/veterinaria/mascotas?id=${id}&nombre=${nombre}&idRaza=${idRaza}&idPropietario=${idPropietario}&fechaNacimiento=${formattedDate}`,
+      {},
       {
         signal: controller.signal,
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
       }
     ),
     controller,
